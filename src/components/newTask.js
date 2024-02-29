5a8d6711f897a39e1f39103485f55d134be91192
Hojoon Kim <hojoon0724@gmail.com>
2024-02-29 11:58:06 -0800

    updated 'add new task' layout - reduce clicks and keep the ui consistent

--- src/components/newTask.js
+++ src/components/newTask.js
@@ -1,25 +1,23 @@
-import {Form} from "react-router-dom"
+import { Form } from 'react-router-dom'
 
-function NewTask({handleNewTask}) {
-    return (
-        <Form onSubmit={handleNewTask} className='new-task-form flex'>
-                <div className="task-field flex">
-                    <label> Description</label>
-                    <textarea className="border-corner" name='task' placeholder="Enter task description"/>
-                </div>
-                <div className="task-field flex">
-                    <label>Priority</label>
-                    <select name='priority' className="border-corner">
-                        <option value="1">High</option>
-                        <option value="2">Medium</option>
-                        <option value="3">Low</option>
-                    </select>
-                </div>
-                <div >
-                    <input className="task-field border-corner create-task-button" type='submit' value='Create Task' />
-                </div>
-        </Form>
-    )
+function NewTask({ handleNewTask }) {
+  return (
+    <div className="new-task-container">
+      <Form onSubmit={handleNewTask} className="new-task-form flex">
+        <div className="new-task-label">New task: </div>
+        <input className="task-name" name="task" />
+        <div className="new-task-label">Priority:</div>
+        <select name="priority" className="border-corner">
+          <option value="1">High</option>
+          <option value="2">Medium</option>
+          <option value="3" selected="selected">
+            Low
+          </option>
+        </select>
+        <input type="submit" value="Create New Task" />
+      </Form>
+    </div>
+  )
 }
 
 export default NewTask
